cd8e75299f9b8458b78306c764a682ca60085708
Shibo Tong <shibo.tong@icloud.com>
2024-07-24 20:58:59 +1000

    add bearer token

--- Sources/App/BearerAuthMiddleWare.swift
+++ Sources/App/BearerAuthMiddleWare.swift
@@ -5,4 +5,22 @@
 //  Created by Shibo Tong on 24/7/2024.
 //
 
-import Foundation
+import Vapor
+
+struct BearerAuthMiddleWare: AsyncMiddleware {
+    func respond(to request: Request, chainingTo next: any AsyncResponder) async throws -> Response {
+        guard let bearerToken = request.headers.bearerAuthorization?.token else {
+            throw Abort(.unauthorized, reason: "A bearer token is required")
+        }
+        guard tokens.contains(bearerToken) else {
+            throw Abort(.unauthorized, reason: "Invalid token")
+        }
+        
+        return try await next.respond(to: request)
+    }
+}
+
+let tokens: [String] = [
+    "sk-ollama-bcd373bc7ac8a87779df02b735018d73",
+    "sk-ollama-7ca4d727455b829cb4ba0bce5208719d"
+]
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,32 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
+    app.get("**") { req async throws in
+        return try await req.ollamaRedirect()
     }
+    
+    app.post("**") { req async throws in
+        return try await req.ollamaRedirect()
+    }
+    
+    app.delete("**") { req async throws in
+        return try await req.ollamaRedirect()
+    }
+    
+    app.get("") { req async throws in
+        return try await req.ollamaRedirect()
+    }
+}
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+let baseURL = "http://192.168.1.143:11434"
+
+extension Request {
+    var redirectURI: URI {
+        let redirectURL = "\(baseURL)\(self.url.path)"
+        return URI(string: redirectURL)
+    }
+    
+    func ollamaRedirect() async throws -> ClientResponse {
+        return try await self.client.send(self.method, to: redirectURI, beforeSend: { try $0.content.encode(self.body.description) })
     }
 }
