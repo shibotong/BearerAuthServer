ee80f9138da3636a45489ec63423d3275a844e83
Shibo Tong <shibo0617@gmail.com>
2024-07-24 21:34:25 +1000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,9 +2,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "bearerReverse",
+    name: "BearerAuthServer",
     platforms: [
-       .macOS(.v13)
+        .macOS(.v13)
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
